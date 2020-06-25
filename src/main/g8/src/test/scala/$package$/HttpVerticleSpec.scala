4dc609abd27d9570decf8825fb344954306f2b6b
codepitbull <jochen@codepitbull.de>
2020-06-25 20:46:28 +0200

    Fix HttpVerticleSpec

--- src/main/g8/src/test/scala/$package$/HttpVerticleSpec.scala
+++ src/main/g8/src/test/scala/$package$/HttpVerticleSpec.scala
@@ -1,14 +1,14 @@
 package $package$
 
-import io.vertx.scala.ext.web.client._
+import io.vertx.scala.core._
 
 import org.scalatest.matchers.should.Matchers
 
 class HttpVerticleSpec extends VerticleTesting[HttpVerticle] with Matchers {
 
   "HttpVerticle" should "bind to 8666 and answer with 'world'" in {
-    vertx.createHttpClient()
-      .getNowFuture(8666, "127.0.0.1", "/hello")
+    vertx.createHttpClient
+      .getFuture(8666, "127.0.0.1", "/hello")
       .flatMap(_.bodyFuture)
       .map(_.toString("UTF-8") should equal("world"))
   }
